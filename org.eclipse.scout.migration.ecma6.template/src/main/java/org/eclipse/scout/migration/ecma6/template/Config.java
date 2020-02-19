404c5d64205f06db5c74f78a23382f8e8df97427
Claudio Guglielmo <claudio.guglielmo@bsiag.com>
2020-02-19 17:09:41 +0100

    Update config

--- org.eclipse.scout.migration.ecma6.template/src/main/java/org/eclipse/scout/migration/ecma6/template/Config.java
+++ org.eclipse.scout.migration.ecma6.template/src/main/java/org/eclipse/scout/migration/ecma6/template/Config.java
@@ -10,7 +10,9 @@ import org.eclipse.scout.rt.platform.Replace;
 public class Config extends Configuration {
 
 	/**
-	 * Specifies the path to your project to migrate.
+	 * Specifies the absolute path to the Maven module that should be migrated.
+	 * If you have a Scout Classic application this is the module that ends with .ui.html.
+	 * If you have a Scout JS application there may be 2 modules: .ui and .ui.app. Just migrate the modules one after another.
 	 */
 	@Override
 	public Path getSourceModuleDirectory() {
@@ -18,6 +20,11 @@ public class Config extends Configuration {
 		return Paths.get("C:/path/to/your/project/com.abc.ui.html");
 	}
 
+	/**
+	 * Specifies where the migrated files should be stored.
+	 * Just return getSourceModuleDirectory() if you want to perform an in-place migration
+	 * and commit the changes to VCS afterwards (regular migration).
+	 */
 	@Override
 	public Path getTargetModuleDirectory() {
 		return getSourceModuleDirectory();
@@ -25,22 +32,52 @@ public class Config extends Configuration {
 
 	/**
 	 * Specifies the folder name where your JS and CSS files are stored, e.g. com.abc.ui.html/src/main/js/--> folder <--
+	 * <p>
+	 * If you don't have the folder because you don't have any JS or CSS files you can omit it.
 	 */
 	@Override
 	public String getJsFolderName() {
 		// TODO adjust
-		return "folder";
+		return "abc";
 	}
 
 	/**
 	 * Specifies the namespace used by your JS file. It is typically the same value as for {@link #getJsFolderName()}.
-	 * The namespace is defined in your JS module (e.g. abc-module.js).
-	 * If you don't have any JS files you can omit it.
+	 * The namespace is defined in your JS module (e.g. abc-module.js) and used in your JS classes
+	 * (e.g. If you have a class called abc.MyClass.prototype, then the namespace is abc).
+	 * <p>
+	 * If you don't have any JS or CSS files you can omit it.
 	 */
 	@Override
 	public String getNamespace() {
 		// TODO adjust
-		return "namespace";
+		return "abc";
+	}
+
+	/**
+	 * The application will create an API file for your project too and put it into the api folder. 
+	 * This property specifies the name of your module under migration which you will later put in the package.json.
+	 * (see also https://eclipsescout.github.io/10.0/migration-guide.html#build-stack-migration, Step 4 and 5).
+	 * It will be used for import generation from another module or from the specs.
+	 * The scope (@abc) can be your application name, your company name or whatever you like.
+	 * <p>
+	 * If you don't have any JS or CSS files you can omit it.
+	 */
+	@Override
+	public String getPersistLibraryName() {
+		// TODO adjust
+		return "@abc/ui";
+	}
+
+	/**
+	 * Specifies the name of the file that contains your api, choose what you want.
+	 * <p>
+	 * If you don't have any JS or CSS files you can omit it.
+	 */
+	@Override
+	public String getPersistLibraryFileName() {
+		// TODO adjust
+		return "abc.json";
 	}
 
 
